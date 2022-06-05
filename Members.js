531c6cad83c8a368434f74b7c42a78bfe6006f05
Mochamad Rizal Hidayat <rizalwednesday@gmail.com>
2022-06-05 16:35:50 +0300

    Tutorial Express JS

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        status: 'active' 
+    },
+    {
+        id: 2,
+        name: 'Shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active' 
+    },
+    {
+        id: 3,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive' 
+    }
+];
+
+module.exports = members;
